24cf22838daf74ae34a52c8073687fd30a44c7cf
dmitryhusev <dimtri.gusev@gmail.com>
2020-12-19 00:18:08 +0200

    .

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -5,7 +5,6 @@ from screen import make_attachment
 @pytest.mark.smoke
 def test_smoke_1(browser):
     browser.get('https://pytest-html.readthedocs.io/en/latest/user_guide.html')
-    make_attachment(browser)
     assert 1
 
 
