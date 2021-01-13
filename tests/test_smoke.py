7e5c4232d037dee46e4ed3fb302f426bd197e0ff
dmitryhusev <dimtri.gusev@gmail.com>
2021-01-13 10:27:28 +0200

    .

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.smoke
 def test_smoke_1(browser):
     browser.get('https://pytest-html.readthedocs.io/en/latest/user_guide.html')
-    assert 1
+    assert 0
 
 
 @pytest.mark.smoke
