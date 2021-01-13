7a94236cef0c23201833a36c7285a219ca457ad6
dmitryhusev <dimtri.gusev@gmail.com>
2021-01-13 12:30:44 +0200

    .

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,10 +4,10 @@ import pytest
 @pytest.mark.smoke
 def test_smoke_1(browser):
     browser.get('https://pytest-html.readthedocs.io/en/latest/user_guide.html')
-    assert 0
+    assert 1
 
 
 @pytest.mark.smoke
 def test_smoke_2(browser):
     browser.get('https://alib.com.ua')
-    assert 0
+    assert 1
