2090d69c195d1e4005d1305165242b46b99d8cc5
dmitryhusev <dimtri.gusev@gmail.com>
2021-01-15 11:28:37 +0200

    settings updated

--- app/src/settings.py
+++ app/src/settings.py
@@ -5,10 +5,10 @@ TIMEOUT = 30
 RUN_LOCALLY = True
 
 URLS = {
-    'stage': 'https://staging.emissary.io/'
+    'stage': 'https://your.site.url/'
 }
 
 BASE_URL = os.getenv('ENV') if os.getenv('ENV') else URLS.get('stage')
 
-USER_EMAIL = 'dmytro@emissary.io'
+USER_EMAIL = ''
 USER_PASSWORD = os.getenv('USER_PASSWORD')
